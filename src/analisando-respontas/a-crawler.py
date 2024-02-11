9911f2271d6bb8f8cb53861c2bc2cab1d4c60b61
uelionobre <uelionobre@hotmail.com>
2024-02-10 23:45:57 -0300

    Obtendo um link

--- src/analisando-respontas/a-crawler.py
+++ src/analisando-respontas/a-crawler.py
@@ -5,10 +5,5 @@ url = "https://books.toscrape.com/"
 response = requests.get(url)
 selector = Selector(response.text)
 
-image = selector.css("img.thumbnail").get()
-print(f"Uma imagem:\n{image}", end="\n" * 2)
-
-images = selector.css("img.thumbnail").getall()
-print("Todas as imagens:")
-for thumbnail in images:
-    print(thumbnail)
+image = selector.css("div.image_container a").get()
+print(image)
